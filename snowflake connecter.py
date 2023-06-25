ad598ceec34a5b7e0b85707892dd164f2e13e4cc
priyankastudy80 <137557121+priyankastudy80@users.noreply.github.com>
2023-06-25 23:52:01 +0200

    c

--- snowflake connecter.py
+++ snowflake connecter.py
@@ -10,4 +10,5 @@ conn = snowflake.connector.connect(
     warehouse='COMPUTE_WH'
     )
 
-conn.cursor().execute("put c:\users\pthakar\my_third_file.txt @demo_db.public.my_internal_named_stage")
+conn.cursor().execute("put file:///users/pthakar/my_third_file.txt @demo_db.public.my_internal_named_stage")
+
