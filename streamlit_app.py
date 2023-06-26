2121d50f50816c096c80f87b9c8b541a1c227bf1
priyankastudy80 <137557121+priyankastudy80@users.noreply.github.com>
2023-06-27 00:36:42 +0200

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,9 +53,9 @@ try:
         my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     #my_cur = my_cnx.cursor()
     #my_cur.execute("SELECT * from fruit_load_list")
-        my_data_rows = get_fruit_load_list()
+        my_data_rows = get_fruit_load_list()    
+        streamlit.dataframe(my_data_rows)
     
-    streamlit.dataframe(my_data_rows)
     streamlit.stop()
     def insert_row_snowflake(new_fruit):
         with my_cnx.cursor() as my_cur:
